d28464d8ac4b7d1b31397b79881cbaeea6ebe403
KalvinVilla <villa.kalvin@gmail.com>
2024-04-18 11:29:44 +0200

    feat(args): add argsparse system

---
+++ src/arguments/index.py
@@ -0,0 +1,4 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="A command-line tool to help you to have compliance with your commit & easy the versioning.")
+subparsers = parser.add_subparsers(dest='command', help='Sub-command help')
---
+++ src/main.py
@@ -0,0 +1,18 @@
+from config import init_config
+from arguments.index import parser
+
+NAME="GitBenchmark"
+VERSION="0.1.0"
+
+def help_function():
+    print(f"{NAME} {VERSION}")
+    parser.print_help()
+
+if __name__ == "__main__":
+    init_config()
+    parser.add_argument('--version', action='version', version=f"{NAME} {VERSION}")
+    args = parser.parse_args()
+    if 'func' in args:
+        args.func(args)
+    else:
+        help_function()
